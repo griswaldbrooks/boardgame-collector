d310426e637f33cc160198de3fd5a0b3c1f95766
Griswald Brooks <griswald.brooks@gmail.com>
2026-08-15 17:35:04 -0400

    no-mistakes(review): Inset content from system bars natively, drop unused serde

--- src-tauri/gen/android/app/src/main/java/home/bgn/coordinator/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/home/bgn/coordinator/MainActivity.kt
@@ -1,11 +1,18 @@
 package home.bgn.coordinator
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    val content = findViewById<View>(android.R.id.content)
+    ViewCompat.setOnApplyWindowInsetsListener(content) { view, insets ->
+      val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      view.setPadding(bars.left, bars.top, bars.right, bars.bottom)
+      insets
+    }
   }
 }
